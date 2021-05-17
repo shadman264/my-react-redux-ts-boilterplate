251919cac6cec6ae704c86ee4cd46e9b352e4103
shadman264 <shadmanmahdi264@gmail.com>
2021-05-16 22:58:36 -0400

    Update store.tsx

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -4,7 +4,7 @@
  createStore: Creates a Redux store that holds the state tree
  Store: The TS Type used for the store, or state tree
  */
-import { applyMiddleware, combineReducers, createStore, Store } from 'redux';
+import { applyMiddleware, combineReducers, createStore, Store, compose } from 'redux';
 /*  Thunk
 Redux Thunk middleware allows you to write action creators that return a function instead of an action. The thunk can be used to delay the dispatch of an action, or to dispatch only if a certain condition is met. The inner function receives the store methods dispatch and getState as parameters.
 */
@@ -22,8 +22,10 @@ const rootReducer = combineReducers<IAppState>({
   basicState: basicReducer
 });
 
+const composeEnhancer = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 // Create a configure store function of type `IAppState`
 export default function configureStore(): Store<IAppState, any> {
-  const store = createStore(rootReducer, undefined, applyMiddleware(thunk));
+  const store = createStore(rootReducer, undefined, composeEnhancer(applyMiddleware(thunk)));
   return store;
 }
